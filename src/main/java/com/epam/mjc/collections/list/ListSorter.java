1c0b0430e4d3324904b4544e25a2d26ca8e16ed3
Vitalii Baranetskyi <vitalik.baranetskiy@gmail.com>
2022-11-14 00:20:12 +0200

    Solved

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,23 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int aInt = Integer.parseInt(a);
+        int bInt = Integer.parseInt(b);
+        int funcA = 5*aInt*aInt+3;
+        int funcB = 5*bInt*bInt+3;
+        if(funcA > funcB){
+            return 1;
+        } else if(funcA == funcB){
+            return Integer.compare(aInt, bInt);
+        } else{
+            return -1;
+        }
     }
 }
